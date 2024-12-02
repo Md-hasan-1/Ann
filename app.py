0e2cd92a2c001b5ff7ba15cb33ca7aa31ff63674
Md-hasan-1 <hasanraza768001@gmail.com>
2024-12-02 15:11:24 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import tensorflow as tf
+import pandas as pd
+import pickle
+
+# Load the trained model
+model = tf.keras.models.load_model(
+    'C:/Users/hasan/Documents/DS/Udemy/DL/ANN/Classification/artifact/class.h5'
+    )
+
+# Load the encoders and scaler
+with open(
+    "C:/Users/hasan/Documents/DS/Udemy/DL/ANN/Classification/artifact/preprocessor.pkl", 
+    'rb') as file_obj:
+    preprocessor = pickle.load(file_obj)
+
+## streamlit app
+st.title('Customer Churn PRediction')
+
+# User input
+geography = st.selectbox('Geography', ['France', 'Spain', 'Germany'])
+gender = st.selectbox('Gender', ['Female', 'Male'])
+age = st.slider('Age', 18, 92)
+balance = st.number_input('Balance')
+credit_score = st.number_input('Credit Score')
+estimated_salary = st.number_input('Estimated Salary')
+tenure = st.slider('Tenure', 0, 10)
+num_of_products = st.slider('Number of Products', 1, 4)
+has_cr_card = st.selectbox('Has Credit Card', [0, 1])
+is_active_member = st.selectbox('Is Active Member', [0, 1])
+
+# Prepare the input data
+input_data = pd.DataFrame({
+    'CreditScore': [credit_score],
+    'Geography':[geography],
+    'Gender': [gender],
+    'Age': [age],
+    'Tenure': [tenure],
+    'Balance': [balance],
+    'NumOfProducts': [num_of_products],
+    'HasCrCard': [has_cr_card],
+    'IsActiveMember': [is_active_member],
+    'EstimatedSalary': [estimated_salary]
+})
+
+# Scale the input data
+input_data_scaled = preprocessor.transform(input_data)
+
+
+# Predict churn
+prediction = model.predict(input_data_scaled)
+prediction_proba = prediction[0][0]
+
+st.write(f'Churn Probability: {prediction_proba:.2f}')
+
+if prediction_proba > 0.5:
+    st.write('The customer is likely to churn.')
+else:
+    st.write('The customer is not likely to churn.')
